1bf5d2e97bf7c7d11e214e543d6852d0a51f1e1e
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-17 16:00:54 +0100

    Socket functionality stubbed out

--- src/pages/Sockets/index.js
+++ src/pages/Sockets/index.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
+const socket = io('http://localhost:3000/');
 
 const SocketTesting = () => {
+	useEffect(() => {
+		socket.on('userJoined', () => {
+			console.log('welcome');
+		});
+	}, []);
+
+	useEffect(() => {
+		socket.on('userFinished', (quizData) => {
+			console.log('Someone is done', quizData);
+		});
+	}, []);
+
+	const joinRoom = (e) => {
+		e.preventDefault();
+		// store the name of the room.
+		const roomName = e.target.roomName.value;
+		// send event to the socket.
+		socket.emit('joinRoom', roomName);
+	};
+
+	const finishedQuiz = () => {
+		const quizData = {
+			roomName: 'tbc',
+			score: 10,
+		};
+		socket.emit('userFinished', quizData);
+	};
+
 	return (
 		<>
-			<h1>hi</h1>
+			<form onSubmit={joinRoom}>
+				<input id="roomName" type="text" />
+				<input type="submit" />
+			</form>
+			<button onClick={finishedQuiz}>finish the quiz</button>
 		</>
 	);
 };
